e1c2f838682cb5a614f8d286155d73714aaad2f9
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-13 22:07:22 +0900

    詳細ページのルーティングを設定

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+// コンポーネント
 import Header from './component/Parts/Header';
 import Footer from './component/Parts/Footer';
 import PostList from './component/Posts/List'
-import * as serviceWorker from './serviceWorker';
+import PostDetail from './component/Posts/Detail'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Header />
-    <PostList />
+    <Switch>
+      <Route exact path="/" component={PostList} />
+      <Route exact path="/posts/:id" component={PostDetail} />
+    </Switch>
     <Footer />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
