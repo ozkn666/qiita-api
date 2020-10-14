2e9b1fef8d2123e1068a310cb531748af799d3df
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-14 22:56:39 +0900

    アプリからのログアウト機能を実装

--- src/component/Parts/Logout.js
+++ src/component/Parts/Logout.js
@@ -1,8 +1,26 @@
 import React from "react"
+import {TOKEN_NAME} from '../../constants/common'
+import Authentication from '../../modules/Authentication'
 
 const Logout = () => {
+
+  const logoutApp = event => {
+    event.preventDefault()
+
+    // モーダルダイアログを表示し、OKならログアウト処理を実行
+    let isLogut = window.confirm("ログアウトしてよろしいですか？")
+    if (!isLogut) return
+
+    // トークンを削除
+    sessionStorage.removeItem(TOKEN_NAME)
+
+    /* アプリケーションの認証認可を確認する
+    トークンがなくなっているので、認可ページにリダレクトがかかる */
+    Authentication()
+  }
+
   return (
-    <button className="c-btn__sub">Logout</button>
+    <button className="c-btn__sub" onClick={logoutApp}>Logout</button>
   )
 }
 
