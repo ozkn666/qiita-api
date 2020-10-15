82e903012cea34007a0c53aff0d4474584f36361
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-15 09:11:17 +0900

    404ページをマークアップ

--- src/component/404.js
+++ src/component/404.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const NotFound = () =>(
-  <h1>Not Found</h1>
+  <div className="l-main">
+  <div className="p-main">
+    <h2 className="c-heading__title">ページは存在しません。</h2>
+  </div>
+</div>
 )
 
 export default NotFound
