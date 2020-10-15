8ad748270fb3ecf320c41346c5292ca47db6f3f3
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-15 11:17:11 +0900

    投稿詳細ページにページローダーを追加

--- src/component/Posts/Detail/index.js
+++ src/component/Posts/Detail/index.js
@@ -7,38 +7,48 @@ import axios from 'axios'
 import {BASE_URL} from '../../../constants/common'
 import ReactMarkdown from 'react-markdown/with-html'
 import formatDate from '../../../modules/FormatDate'
+import Loading from '../../Parts/Loading'
 
 const PostDetail = (props) => {
   
   // URLから投稿記事のidを取得
   const postId = props.match.params.id
 
+  // 投稿詳細データ
   const [postData, setPostData] = useState({})
+  
+  // ページローダーの状態
+  const [isLoading, setIsLoading] = useState(true)
 
   // マウント時に投稿データを取得する
   useEffect(()=> {
     axios.get(`${BASE_URL}items/${postId}`).then(response => {
       setPostData(response.data)
+      // データを取得し終わったらページローダーを非表示にする
+      setIsLoading(false)
     })
   }, [])
 
   return (
-    <div className="l-main">
-      <div className="p-main">
-        <div className="p-post">
-          <div className="p-post__meta">{formatDate(postData.updated_at)}に更新</div>
-          <div className="p-post__content">
-            <h2 className="c-heading__post-title">{postData.title}</h2>
-            <div className="p-post__body">
-              <ReactMarkdown 
-                source={postData.body}
-                escapeHtml={false}
-              />
+    <>
+      <Loading isActive={isLoading} />
+      <div className="l-main">
+        <div className="p-main">
+          <div className="p-post">
+            <div className="p-post__meta">{formatDate(postData.updated_at)}に更新</div>
+            <div className="p-post__content">
+              <h2 className="c-heading__post-title">{postData.title}</h2>
+              <div className="p-post__body">
+                <ReactMarkdown 
+                  source={postData.body}
+                  escapeHtml={false}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
