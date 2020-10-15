df1962d2fa34fce02e06e3d8f75e183fd49d2300
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-15 14:54:47 +0900

    環境ごとに読み込む定数を切り替える設定を追加

--- src/constants/common.js
+++ src/constants/common.js
@@ -2,10 +2,10 @@
 export const BASE_URL = "https://qiita.com/api/v2/"
 
 // 認証・認可に必要な定数
-export const CLIENT_ID =     "cef3dbae0a412a051da06a6e47a15621e041d7ed"
-export const CLIENT_SECRET = "f6bacfc0b4fb51fbc0369a96faa4b6d9087e34e8"
-export const CLIENT_SCOPE =  "read_qiita"
-export const CLIENT_STAT =   "ABC"
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+export const CLIENT_SCOPE = "read_qiita"
+export const CLIENT_STAT = "ABC"
 
 // Qiitaの認可ページURL
 export const AUTH_URL = 'https://qiita.com/api/v2/oauth/authorize'
