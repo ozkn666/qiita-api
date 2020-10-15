309381732a64a629cd7eb29fa02758ab4e03852f
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-10-15 11:16:57 +0900

    ページローダーの処理を早期リターンに修正

--- src/component/Parts/Loading.js
+++ src/component/Parts/Loading.js
@@ -1,10 +1,17 @@
+/**
+ * ページローダーのコンポーネント
+ * propsで渡されるisActiveがtrueの時ページローダーを表示する
+ */
+
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 
-
 const Loading = props => {
-  console.log(props)
+
+  // 非表示時は空の要素を返す
+  if (!props.isActive) return <></>
+
   return (
     <div className="c-loading">
       <FontAwesomeIcon icon={faSync} size="6x" spin />
